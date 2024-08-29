a2682fdc126b3e72c6183ce20c4cb0bf25127927
Anchal Kumar <anchalkmr007@gmail.com>
2024-08-29 20:16:50 +0530

    Experiment with setting the default date as current

--- src/Components/MainLayout/DatePicker/DatePicker.js
+++ src/Components/MainLayout/DatePicker/DatePicker.js
@@ -1,10 +1,25 @@
+import { useEffect , useRef } from "react";
+
 import styles from "./DatePicker.module.css";
 
 const DatePicker = () => {
+  const datePickerRef = useRef();
+  // TODO: Set date picker as current date
+  // Problem: With this, changing the date is not working
+  // useEffect(() => {
+  //   const today = new Date();
+  //   const year = today.getFullYear();
+  //   const month = today.getMonth() >= 10 ? today.getMonth() : "0" + today.getMonth();
+  //   const date = today.getDate();
+  //   const dateString = `${year}-${month}-${date}`;
+  //   console.log(dateString);
+  //   datePickerRef.current.value = dateString;
+  // }, []);
+
   return (
     <div className={styles["date-picker"]}>
-      <label for="date-picker">Choose A Date</label>
-      <input type="date" name="date-picker"></input>
+      <label htmlFor="date-picker">Choose A Date</label>
+      <input type="date" name="date-picker" value={new Date(2024, 7, 1)} ref={datePickerRef}></input>
     </div>
   );
 };
