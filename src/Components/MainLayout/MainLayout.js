08504441ff8a99bcc101102c63cb726e930c1f5a
Anchal Kumar <anchalkmr007@gmail.com>
2024-09-02 13:35:15 +0530

    Lift state of expenses

--- src/Components/MainLayout/MainLayout.js
+++ src/Components/MainLayout/MainLayout.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 import DatePicker from "./DatePicker/DatePicker";
 import Expenses from "./Expenses/Expenses";
 import AddExpense from "./AddExpense/AddExpense";
+import DUMMY_EXPENSES from "./../../utils/dummyExpenses";
 
 import styles from "./MainLayout.module.css";
-import { useState } from "react";
 
 const MainLayout = () => {
   const [pickedDate, setPickedDate] = useState(new Date());
+  const [expenseList, setExpenseList] = useState(DUMMY_EXPENSES);
+
+  const displayList = expenseList.filter(expense => {
+    return expense.date.getTime() === pickedDate.getTime();
+  });
+
   const updatePickedDate = (newDate) => {
     // Edge case when user selects clear option
     if (!newDate) {
@@ -18,10 +26,11 @@ const MainLayout = () => {
     newPickedMonth -= 1; // adjust difference between date from datePicker and array indexing of months in js
     setPickedDate(new Date(newPickedYear, newPickedMonth, newPickedDate));
   };
+
   return (
     <div className={styles["main-layout"]}>
       <DatePicker pickedDate={pickedDate} updatePickedDate={updatePickedDate} />
-      <Expenses pickedDate={pickedDate} />
+      <Expenses displayList={displayList} />
       <AddExpense pickedDate={pickedDate} />
     </div>
   );
---
+++ src/utils/dummyExpenses.js
@@ -0,0 +1,116 @@
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    date: new Date(2024, 7, 29),
+    amount: 100,
+    paymentMode: "Cash",
+    reason: "Milk"
+  },
+  {
+    id: 2,
+    date: new Date(2024, 7, 25),
+    amount: 150,
+    paymentMode: "Cash",
+    reason: "Eggs"
+  },
+  {
+    id: 3,
+    date: new Date(2024, 7, 29),
+    amount: 249,
+    paymentMode: "UPI",
+    reason: "Food"
+  },
+  {
+    id: 4,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 5,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 6,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 7,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 8,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 9,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 10,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 11,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 12,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 13,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 14,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 15,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+  {
+    id: 16,
+    date: new Date(2024, 7, 25),
+    amount: 1000,
+    paymentMode: "UPI",
+    reason: "Party"
+  },
+];
+
+export default DUMMY_EXPENSES;
