3ad6e308f5ce9116aaca958c659eb5cafee7d1f6
Anchal Kumar <anchalkmr007@gmail.com>
2024-09-18 20:20:27 +0530

    Add form data handling

--- src/Components/Authentication/AuthForm.js
+++ src/Components/Authentication/AuthForm.js
@@ -2,51 +2,67 @@ import { useReducer } from "react";
 import styles from "./AuthForm.module.css";
 
 const authFormReducer = (authFormData, action) => {
+  if (action.type === "edit") {
+    return {
+      ...authFormData,
+      [action.field]: action.value,
+    };
+  } else if (action.type === "reset") {
+    return action.data;
+  }
+};
 
-}
-
-const AuthForm = ({ activeForm }) => {
-  const initialFormData = {
+const AuthForm = ({ activeForm , handleIsUserLoggedIn }) => {
+  let initialFormData = {
     email: "",
     password: "",
-  }
-  if(activeForm === "signup") {
+  };
+  if (activeForm === "signup") {
     initialFormData.confirmPassword = "";
     initialFormData.name = "";
   }
-  const [authFormData, authFormDispatchFn] = useReducer(authFormReducer, initialFormData);
+  const [authFormData, authFormDispatchFn] = useReducer(
+    authFormReducer,
+    initialFormData
+  );
 
   const handleFormFieldChange = (event) => {
-
-  }
+    authFormDispatchFn({
+      type: "edit",
+      field: event.target.name,
+      value: event.target.value,
+    });
+  };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    console.log(authFormData);
+    handleIsUserLoggedIn(true);
+  };
 
-  }
+  const handleOnReset = () => {
+    authFormDispatchFn({
+      type: "reset",
+      data: initialFormData,
+    });
+  };
 
-  const handleOnCancel = () => {
+  const handleFormError = () => {};
 
-  }
-
-  const handleFormError = () => {
-
-  }
-  
   return (
     <div className={styles["form-content-container"]}>
       <form onSubmit={handleFormSubmit}>
-        <div className={styles["form-fields"]}>    
-          {activeForm === "signup" &&           
+        <div className={styles["form-fields"]}>
+          {activeForm === "signup" && (
             <div className={styles["form-field"]}>
               <label htmlFor="name">Name</label>
               <input
                 name="name"
                 onChange={handleFormFieldChange}
                 value={authFormData.name}
-                />
+              />
             </div>
-          }
+          )}
           <div className={styles["form-field"]}>
             <label htmlFor="email">Email</label>
             <input
@@ -66,7 +82,7 @@ const AuthForm = ({ activeForm }) => {
               minLength={5}
             />
           </div>
-          {activeForm === "signup" &&           
+          {activeForm === "signup" && (
             <div className={styles["form-field"]}>
               <label htmlFor="confirmPassword">Confirm Password</label>
               <input
@@ -75,16 +91,18 @@ const AuthForm = ({ activeForm }) => {
                 onChange={handleFormFieldChange}
                 value={authFormData.confirmPassword}
                 minLength={5}
-                />
+              />
             </div>
-          }
+          )}
         </div>
         {/* {formError && <div className={styles["form-error"]}>{formError}</div>} */}
         <div className={styles["form-buttons"]}>
-          <button type="reset" onClick={handleOnCancel}>
+          <button type="reset" onClick={handleOnReset}>
             Reset
           </button>
-          <button type="submit">{activeForm === "login" ? "Login" : "Signup"}</button>
+          <button type="submit">
+            {activeForm === "login" ? "Login" : "Signup"}
+          </button>
         </div>
       </form>
     </div>
--- src/Components/Authentication/FormLayout.js
+++ src/Components/Authentication/FormLayout.js
@@ -2,23 +2,36 @@ import { useState } from "react";
 import styles from "./FormLayout.module.css";
 import AuthForm from "./AuthForm";
 
-const FormLayout = (props) => {
+const FormLayout = ({ handleIsUserLoggedIn }) => {
   const [activeForm, setActiveForm] = useState("login");
   const handleActiveForm = (value) => {
     setActiveForm(value);
-  }
+  };
 
   return (
     <div className={styles["form-layout"]}>
       <div className={styles["form-box"]}>
         <div className={styles["form-selection-box"]}>
-          <button className={`${activeForm == "login" ? styles.active : ""}`} onClick={() => handleActiveForm("login")}>Login</button>
-          <button className={`${activeForm == "signup" ? styles.active : ""}`} onClick={() => handleActiveForm("signup")}>Signup</button>
+          <button
+            className={`${activeForm == "login" ? styles.active : ""}`}
+            onClick={() => handleActiveForm("login")}
+          >
+            Login
+          </button>
+          <button
+            className={`${activeForm == "signup" ? styles.active : ""}`}
+            onClick={() => handleActiveForm("signup")}
+          >
+            Signup
+          </button>
         </div>
-        <AuthForm activeForm={activeForm}/>
+        <AuthForm
+          activeForm={activeForm}
+          handleIsUserLoggedIn={handleIsUserLoggedIn}
+        />
       </div>
     </div>
-  )
+  );
 };
 
 export default FormLayout;
