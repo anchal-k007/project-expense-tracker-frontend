63beb82f37609b8e11f2c8449fc272738a798e19
Anchal Kumar <anchalkmr007@gmail.com>
2024-10-21 18:55:47 +0530

    Create total expenses chart

--- src/Components/MainLayout/AnalysisCharts/AllCharts.js
+++ src/Components/MainLayout/AnalysisCharts/AllCharts.js
@@ -1,6 +1,7 @@
 import styles from "./AllCharts.module.css";
 import BarChart from "./ChartComponents/BarChart";
 import DisplayBlock from "./ChartComponents/DisplayBlock";
+import TotalExpense from "./DisplayCharts/TotalExpense";
 
 const labels = ["January", "February", "March", "April", "May", "June"];
 const datasets = [
@@ -29,6 +30,7 @@ const AllCharts = ({ isLoading = false, title, fetchedData = [] }) => {
       <h1>{displayText}</h1>
       {!isLoading && fetchedData && fetchedData.length !== 0 && (
         <div className={styles["all-charts-flexbox"]}>
+          <TotalExpense expenses={fetchedData} />
           <BarChart labels={labels} datasets={datasets} />
           <DisplayBlock title="Test" data="100" />
           <BarChart labels={labels} datasets={datasets} />
